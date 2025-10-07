c3e0745b9be5449f8c76080a6bc8e330bd8f3247
Alberto Flores <alberto.flores@ucb.edu.bo>
2025-10-07 12:50:28 -0400

    ya envio mensajes con el forms

--- src/app/views/dashboard/footer/footer.component.ts
+++ src/app/views/dashboard/footer/footer.component.ts
@@ -1,12 +1,31 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import emailjs, { type EmailJSResponseStatus } from '@emailjs/browser';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.css'
 })
 export class FooterComponent {
+  private serviceID = 'service_g8wlulr';
+  private templateID = 'template_fykrhz5';
+  private publicKey = 'lTpZru93uuLI8Mp-z';
 
+  public sendEmail(formData: any) {
+    const templateParams = {
+      ...formData,
+    };
+
+    emailjs.send(this.serviceID, this.templateID, templateParams, this.publicKey)
+      .then((response: EmailJSResponseStatus) => {
+        console.log('SUCCESS!', response.status, response.text);
+        alert('Message sent successfully!');
+      }, (error) => {
+        console.log('FAILED...', error);
+        alert('Message failed to send. Please try again or contact me directly.');
+      });
+  }
 }
